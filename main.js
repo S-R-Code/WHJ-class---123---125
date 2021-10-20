e096af049375f52331f9b839780bb0088d6d9bdf
S-R-Code <83865650+S-R-Code@users.noreply.github.com>
2021-10-20 12:54:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+ video = createCapture(VIDEO);
+ video.size(550,500);
+
+ canvas = createCanvas(550, 550);
+ canvas.position(560, 150);
+
+ poseNet = ml5.poseNet( video, modelLoaded);
+ poseNet.on('pose', gotPoses);
+}
+function draw(){
+ background('gray');
+ fill('purple');
+ stroke('blue');
+ square(noseX, noseY, difference);
+ document.getElementById("square_side").innerHTML = "Width and Height of a square will be = " + difference + "px";
+}
+function modelLoaded(){
+ console.log('ModelLoaded!');
+}
+function gotPoses(results){
+    if (results.length > 0) {
+      console.log(results);
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("noseX = " + noseX , "noseY = " + noseY);
+
+      rightWristX = results[0].pose.rightWrist.x;
+      leftWristX = results[0].pose.leftWrist.x;
+      difference = floor(leftWristX - rightWristX);
+
+      console.log( "leftWristX = " + leftWristX, "rightWristX = " + rightWristX , "difference = " + difference );
+
+    }
+
+}
+
